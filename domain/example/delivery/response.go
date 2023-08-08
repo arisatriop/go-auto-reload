02705f442b7646a67a48ce93b6ed28cadeac88fa
Ari Satrio <ari.satrio@uii.ac.id>
2023-08-08 22:07:22 +0700

    add response

--- domain/example/delivery/response.go
+++ domain/example/delivery/response.go
@@ -1,1 +1,34 @@
 package delivery
+
+import "github.com/arisatriop/go-auto-reload/domain/example/entity"
+
+type Response struct {
+	Code    string `json:"code"`
+	Example string `json:"example"`
+	Uuid    string `json:"uuid"`
+}
+
+type ExampleResponse interface {
+	ResponseFindAll(ex []entity.Example) []Response
+	ResponseFindById(ex *entity.Example) *Response
+	ResponseFindCode(ex *entity.Example) *Response
+	ResponseFindByUuid(ex *entity.Example) *Response
+}
+
+type ExampleResponseImpl struct{}
+
+func (r *ExampleResponseImpl) ResponseFindAll(ex []entity.Example) []Response {
+	panic("Not implement")
+}
+
+func (r *ExampleResponseImpl) ResponseFindById(ex *entity.Example) *Response {
+	panic("Not implement")
+}
+
+func (r *ExampleResponseImpl) ResponseFindCode(ex *entity.Example) *Response {
+	panic("Not implement")
+}
+
+func (r *ExampleResponseImpl) ResponseFindByUuid(ex *entity.Example) *Response {
+	panic("Not implement")
+}
