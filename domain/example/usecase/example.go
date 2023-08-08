76c172366867aaafdf9f71c263f5272fc8972243
arsatriop <arisatrioputra03@gmail.com>
2023-08-09 05:55:04 +0700

    add usecase

--- domain/example/usecase/example.go
+++ domain/example/usecase/example.go
@@ -1,1 +1,49 @@
 package usecase
+
+import (
+	"github.com/arisatriop/go-auto-reload/domain/example/delivery"
+	"github.com/arisatriop/go-auto-reload/domain/example/repository"
+	"github.com/arisatriop/goerr"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+type ExampleUsecase interface {
+	Create(ctx *gin.Context, request *delivery.ExampleCreateRequest) *goerr.Error
+	Update(ctx *gin.Context, request *delivery.ExampleUpdateRequest) *goerr.Error
+	Delete(ctx *gin.Context, id string) *goerr.Error
+	FindById(ctx *gin.Context, id string) (*delivery.Response, *goerr.Error)
+	FindAll(ctx *gin.Context, request *delivery.ExampleReadRequest) ([]delivery.Response, *goerr.Error)
+}
+
+type ExampleUsecaseImpl struct {
+	Validator  *validator.Validate
+	Repository repository.ExampleRepository
+}
+
+func (u *ExampleUsecaseImpl) Create(ctx *gin.Context, request *delivery.ExampleCreateRequest) *goerr.Error {
+	panic("Not implement")
+}
+
+func (u *ExampleUsecaseImpl) Update(ctx *gin.Context, request *delivery.ExampleUpdateRequest) *goerr.Error {
+	panic("Not implement")
+}
+
+func (u *ExampleUsecaseImpl) Delete(ctx *gin.Context, id string) *goerr.Error {
+	panic("Not implement")
+}
+
+func (u *ExampleUsecaseImpl) FindById(ctx *gin.Context, id string) (*delivery.Response, *goerr.Error) {
+	panic("Not implement")
+}
+
+func (u *ExampleUsecaseImpl) FindAll(ctx *gin.Context, request *delivery.ExampleReadRequest) ([]delivery.Response, *goerr.Error) {
+	panic("Not implement")
+}
+
+func NewExampleUsecase(validator *validator.Validate, repository repository.ExampleRepository) ExampleUsecase {
+	return &ExampleUsecaseImpl{
+		Validator:  validator,
+		Repository: repository,
+	}
+}
