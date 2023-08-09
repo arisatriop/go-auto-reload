032ac19f4d4fb2fe0b393e9c15f91c6f5ac1e7d8
arsatriop <arisatrioputra03@gmail.com>
2023-08-09 07:05:17 +0700

    add routes example

--- routes/routes.go
+++ routes/routes.go
@@ -1,11 +1,21 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/arisatriop/go-auto-reload/config"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+
+	example "github.com/arisatriop/go-auto-reload/domain/example/delivery"
+)
 
 type RouteGroup struct {
 	Public *gin.RouterGroup
 }
 
 func InitRoute(gin *gin.Engine) {
+	db := config.GetDBConnection()
+	validator := validator.New()
 
+	example := example.Route{}
+	example.Init(gin, validator, db, "/example")
 }
