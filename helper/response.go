0d78f7b5be563345026e6449581b9a795cc3c921
arsatriop <arisatrioputra03@gmail.com>
2023-08-10 16:50:17 +0700

    adda response helper

--- helper/response.go
+++ helper/response.go
@@ -1,1 +1,45 @@
 package helper
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type ResponseBody struct {
+	Message interface{} `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+type ResponseBodyWithCount struct {
+	Message interface{} `json:"message"`
+	Count   interface{} `json:"count"`
+	Limit   interface{} `json:"limit"`
+	Offset  interface{} `json:"offset"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+func StatusOK(ctx *gin.Context, data interface{}, m ...interface{}) {
+
+	res := ResponseBody{
+		Message: "sukses",
+		Data:    data,
+	}
+	if len(m) > 0 {
+		res.Message = m[0]
+	}
+
+	ctx.JSON(http.StatusOK, res)
+}
+
+func StatusOKwithCount(ctx *gin.Context, data *ResponseBodyWithCount, m ...interface{}) {
+	ctx.JSON(http.StatusOK, data)
+}
+
+func StatusNotOKV2(ctx *gin.Context, err *Error) {
+	res := ResponseBody{
+		Message: err.Message,
+		Data:    err.Data,
+	}
+	ctx.JSON(err.Code, res)
+}
